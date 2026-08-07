f9f84bdabbc7bf3a8241509cf2424f3ec317730d
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-07 17:36:09 +0530

    Uncomment FirebaseConfigurations class and methods

--- src/main/java/com/ChatApp/FirebaseConfigurations.java
+++ src/main/java/com/ChatApp/FirebaseConfigurations.java
@@ -1,39 +1,39 @@
-// package com.ChatApp;
+package com.ChatApp;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
-// import jakarta.annotation.PostConstruct;
-// import java.io.ByteArrayInputStream;
-// import java.io.InputStream;
+import jakarta.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-// @Configuration
-// public class FirebaseConfigurations {
-
-
+@Configuration
+public class FirebaseConfigurations {
 
 
-//     @PostConstruct
-//     public void init() throws IOException {
-//         String firebaseConfig = System.getenv("FIREBASE_CONFIG");
 
-//         if (firebaseConfig == null) {
-//             throw new IllegalStateException("FIREBASE_CONFIG env variable not set");
-//         }
 
-//         InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+    @PostConstruct
+    public void init() throws IOException {
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
 
-//         FirebaseOptions options = new FirebaseOptions.Builder()
-//                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                 .build();
+        if (firebaseConfig == null) {
+            throw new IllegalStateException("FIREBASE_CONFIG env variable not set");
+        }
 
-//         if (FirebaseApp.getApps().isEmpty()) {
-//             FirebaseApp.initializeApp(options);
-//         }
-//     }
-// }
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
