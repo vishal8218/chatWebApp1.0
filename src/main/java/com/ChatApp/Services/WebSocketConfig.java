0dad3c12605c451ae0d34b9751dcab6e423b56ac
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-27 00:53:40 +0530

    Refactor WebSocketConfig for improved clarity

--- src/main/java/com/ChatApp/Services/WebSocketConfig.java
+++ src/main/java/com/ChatApp/Services/WebSocketConfig.java
@@ -1,11 +1,9 @@
 package com.ChatApp.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,32 +11,30 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	
-	 @Autowired
-	    private JwtAuthChannelInterceptor jwtAuthChannelInterceptor;
-	 
-	 @Override
-	    public void configureClientInboundChannel(ChannelRegistration registration) {
-	        registration.interceptors(jwtAuthChannelInterceptor);
-	    }
 
+	@Autowired
+	private JwtAuthChannelInterceptor jwtAuthChannelInterceptor;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/queue","/topic"); // use queue for private messages
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user"); // prefix for user-specific
-    }
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(jwtAuthChannelInterceptor);
+	}
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
-    	//registry.addEndpoint("/ws");
-    }
-   
-  
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/queue", "/topic"); // use queue for private messages
+		config.setApplicationDestinationPrefixes("/app");
+		config.setUserDestinationPrefix("/user"); // prefix for user-specific
+	}
 
-  
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+				.setAllowedOriginPatterns(
+						"http://localhost:3000",
+						"https://chat-app-lime-iota-22.vercel.app"
+				)
+				.withSockJS();
+	}
 
 }
-
